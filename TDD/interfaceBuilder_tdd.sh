28ca44a6024d62c0144ef60abe89f9362a4215c0
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-04-03 00:56:38 -0300

    Teste adicionar interfaces ao modelo #4
    Testa a opção -a para adicionar interfaces a um modelo ITF. Este teste
    adiciona 7 interfaces em um looping, utilizando os arrays xedge, zedge e
    sfill para adicionar uma interface por vez ao modelo

--- TDD/interfaceBuilder_tdd.sh
+++ TDD/interfaceBuilder_tdd.sh
@@ -27,4 +27,27 @@ TMP=$(mktemp -u tmp_XXXX.itf)
 
 trap "rm $TMP" err exit
 
-error "$($DIR/interfaceBuilder -c $TMP 0 10 0 3 && echo $?)" "0" "1" "Criar novo modelo .itf" 
+error "$($DIR/interfaceBuilder -c $TMP 0 12 0 3.5 && echo $?)" "0" "1" "Criar novo modelo .itf" 
+
+echo "Adicionar 7 interfaces ao modelo..."
+xedge=("0,12" "0,12" "0,2,4,6,8,10,12" "0,2,4,6,8,10,12" "0,2,4,6,8,10,12" "0,2,4,6,8,10,12" "0,2,4,6,8,10,12")
+zedge=("0.3,0.25" "0.7,0.45" "1.2,1.2,1.3,1.4,0.7,1.05,1.2" "1.7,1.7,1.8,2.0,1.1,1.45,1.8" "2.2,2.2,2.3,2.5,1.6,1.9,2.2" "2.7,2.7,2.8,2.9,2.2,2.4,2.7" "3.5,3.5,3.5,3.5,3.5,3.5,3.5")
+
+sfill=("0,0.1,0,0,0.39,0,0" "0,0.4,0,0,0.31,0,0" "0,0.9,0,0,0.28,0,0" "0,1.5,0,0,0.25,0,0" "0,2.0,0,0,0.55,0,0" "0,2.4,0,0,0.44,0,0" "0,3.0,0,0,0.16,0,0")
+
+ERRO="0"
+for i in $(seq 0 1 6)
+do
+	echo "...Interface $i"
+	$DIR/interfaceBuilder -a $TMP x=${xedge[$i]} y=${zedge[$i]} sfill=${sfill[$i]}
+	[ "$?" -ne "0" ] && {
+		ERRO="1"		
+		break
+	}
+done
+
+error "$ERRO" "0" "2" "Adicionar interfaces ao modelo $TMP"
+
+# Remover uma interface da pilha
+#error "" "" "" "Remover uma interface do modelo .itf"
+
