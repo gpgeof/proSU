b3c4a5fe3869c288c97aaf7e005cffa9f46c45f4
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-04-04 02:39:33 -0300

    Esboço da suíte de testes de geomodel #12

---
+++ TDD/geomodel_tdd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# geomodel_tdd.sh (Shell Script)
+# 
+# Objetivo: Testes automatizados do programa geomodel.
+# 
+# Site: https://dirack.github.io
+# 
+# Versão 1.0
+# 
+# Programador: Rodolfo A C Neves (Dirack) 04/04/2020
+# 
+# Email: rodolfo_profissional@hotmail.com
+# 
+# Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+
+source $(dirname $0)/tdd_lib.sh
+
