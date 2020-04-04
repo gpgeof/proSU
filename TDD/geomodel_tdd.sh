17d55c87ed9e76ea55884dcef00200ba59797448
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-04-04 12:21:38 -0300

    Teste gerar modelo do trimodel com geomodel #12
    O programa geomodel é uma interface para gerar modelos com o programa
    trimodel do pacote SU lidos a partir de arquivos itf gerados pelo programa
    interfaceBuilder.
    
    Este teste gera um modelo de 8 interfaces (7 camadas) e tenta gerar o
    modelo binário com geomodel e trimodel.

--- TDD/geomodel_tdd.sh
+++ TDD/geomodel_tdd.sh
@@ -16,3 +16,36 @@
 
 source $(dirname $0)/tdd_lib.sh
 
+# Verificar diretório do programa a ser testado
+DIR=".."
+
+[ -f "./interfaceBuilder" ] && {
+	DIR="."
+}
+
+# Criar novo modelo itf
+TMP=$(mktemp -u tmp_XXXX.itf)
+TMPBIN=$(echo $TMP | cut -d"." -f1)
+TMPBIN="${TMPBIN}.bin"
+
+trap "rm $TMP $TMPBIN" err exit
+
+# Criar modelo de testes e adicionar 7 interfaces
+$DIR/interfaceBuilder -c "$TMP" 0 12 0 3.5 
+
+xedge=("0,12" "0,12" "0,2,4,6,8,10,12" "0,2,4,6,8,10,12" "0,2,4,6,8,10,12" "0,2,4,6,8,10,12" "0,2,4,6,8,10,12")
+zedge=("0.3,0.25" "0.7,0.45" "1.2,1.2,1.3,1.4,0.7,1.05,1.2" "1.7,1.7,1.8,2.0,1.1,1.45,1.8" "2.2,2.2,2.3,2.5,1.6,1.9,2.2" "2.7,2.7,2.8,2.9,2.2,2.4,2.7" "3.5,3.5,3.5,3.5,3.5,3.5,3.5")
+
+sfill=("0.1,0.39" "0.4,0.31" "0.9,0.28" "1.5,0.25" "2.0,0.55" "2.4,0.44" "3.0,0.16")
+
+for i in $(seq 0 1 6)
+do
+	$DIR/interfaceBuilder -a $TMP x=${xedge[$i]} y=${zedge[$i]} sfill=${sfill[$i]}
+	[ "$?" -ne "0" ] && {
+		error "1" "0" "1" "Criar as interfaces do modelo..."
+	}
+done
+
+$DIR/geomodel -m $TMPBIN $TMP
+
+error "$?" "0" "1" "Gerar modelo..."
